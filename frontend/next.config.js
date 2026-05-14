92c11243467a6cb7b53df8cc4a4e7ff23b763028
rudazy <Rrudazy@gmail.com>
2026-05-14 12:30:16 +0100

    fix: exclude pino-pretty and async-storage from webpack bundle

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    // Optional peer deps pulled in transitively by @metamask/sdk and pino.
+    // We don't use them at runtime in the browser, so mark them external
+    // to stop webpack from trying to resolve and bundle them on Vercel.
+    config.externals.push(
+      "pino-pretty",
+      "@react-native-async-storage/async-storage"
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
